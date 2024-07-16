1e772003019369fbacacb10ef0f5b6c9a1db0587
PSKutergin <PSKutergin@gmail.com>
2024-07-16 10:56:31 +0300

    init project and create header component

---
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoryType } from 'src/app/types/category.type';
+import { CategoryService } from '../../services/category.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  categories: CategoryType[] = [];
+
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.categoryService.getCategories()
+      .subscribe((categories: CategoryType[]) => this.categories = categories);
+  }
+
+}
