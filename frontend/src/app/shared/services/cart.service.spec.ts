ef3e6349aa2f99b42b25b632e8ce30dfed0d9ba5
PSKutergin <PSKutergin@gmail.com>
2024-08-14 13:49:33 +0200

    added new test for cart service

--- frontend/src/app/shared/services/cart.service.spec.ts
+++ frontend/src/app/shared/services/cart.service.spec.ts
@@ -1,13 +1,16 @@
 import { of } from "rxjs";
 import { CartService } from "./cart.service";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
 
 describe('cart service', () => {
 
     let cartService: CartService;
+    let valueServiceSpy: jasmine.SpyObj<HttpClient>;
     const countValue = 3;
 
     beforeEach(() => {
-        const valueServiceSpy = jasmine.createSpyObj('HttpClient', ['get']);
+        valueServiceSpy = jasmine.createSpyObj('HttpClient', ['get']);
         valueServiceSpy.get.and.returnValue(of({ count: 3 }));
 
         cartService = new CartService(valueServiceSpy);
@@ -20,5 +23,12 @@ describe('cart service', () => {
         })
 
         cartService.setCount(countValue);
+    })
+
+    it('should make http request for cart data', (done: DoneFn) => {
+        cartService.getCart().subscribe(() => {
+            expect(valueServiceSpy.get).toHaveBeenCalledOnceWith(environment.api + 'cart', { withCredentials: true });
+            done();
+        })
     })
 })
